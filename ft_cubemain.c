58f91c0bc002a76b018be85248fc9c5b749ad72c
Fernando Ballesteros santamaria <fballest@c1r2s3.42madrid.com>
2020-11-25 12:22:36 +0100

    cubemain, abre y comprueba res

--- ft_cubemain.c
+++ ft_cubemain.c
@@ -6,24 +6,35 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 09:21:00 by fballest          #+#    #+#             */
-/*   Updated: 2020/11/24 15:50:45 by fballest         ###   ########.fr       */
+/*   Updated: 2020/11/25 11:12:23 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 
-int			ft_cubemain(t_map *map, t_err *err, t_tex *tex)
+int				ft_cubemain(t_map *map, t_err *err, t_tex *tex)
 {
 	char	*raycast;
 	void	*mlx_ptr;
 	void	*mlx_win;
 
-	if(!(raycast = malloc(sizeof(t_raycast))))
-		return(-1);
+	if (!(raycast = malloc(sizeof(t_raycast))))
+		return (-1);
 	tex->rutano = err->err6;
 	mlx_ptr = mlx_init();
-	mlx_win = mlx_new_window(mlx_ptr, map->rx, map->ry, "cub3D");
+	mlx_get_screen_size(mlx_ptr, &map->grx, &map->gry);
+	//ft_getdefres(map);
+	mlx_win = mlx_new_window(mlx_ptr, map->rx, map->ry, map->name);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
+
+/*void			ft_getdefres(map)
+{
+	if (map->rx == 2560 && map->ry == 1440)
+	{
+		map->rx = &map->grx;
+		map->ry = &map->gry;
+	}
+}*/
