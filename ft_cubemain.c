83cc81c85e006da53081ec08e643fb5edca8806b
Fernando Ballesteros <fballest@student.42madrid.com>
2020-12-04 09:41:09 +0100

    keypress y keyrestore

--- ft_cubemain.c
+++ ft_cubemain.c
@@ -6,7 +6,7 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 09:21:00 by fballest          #+#    #+#             */
-/*   Updated: 2020/12/02 09:16:12 by fballest         ###   ########.fr       */
+/*   Updated: 2020/12/04 09:05:45 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int				ft_cubemain(t_map *map, t_err *err, t_tex *tex)
 {
-	tex->rutano = err->err6;
+	err->err6 = tex->rutano; //OJO ELIMINAR SOLO HABILITADO PARA PASAR TEX
 	map->mlx_ptr = mlx_init();
 	ft_getdefres(map, tex);
 	map->mlx_win = mlx_new_window(map->mlx_ptr, map->rx, map->ry, map->name);
@@ -23,7 +23,6 @@ int				ft_cubemain(t_map *map, t_err *err, t_tex *tex)
 		&map->mlx_sili, &map->mlx_endian);
 	mlx_key_hook(map->mlx_win, ft_key_hook, map);
 	ft_raycasting(map);
-	mlx_hook(map->mlx_win, 17, 1L << 17, ft_exit_game, map);
 	mlx_loop(map->mlx_ptr);
 	return (0);
 }
@@ -73,6 +72,9 @@ void			ft_paint_cei_flo(t_map *map, int x, int y)
 
 int				ft_key_hook(int keycode, t_map *map)
 {
+	mlx_hook(map->mlx_win, 17, 1L << 17, ft_exit_game, map);
+	mlx_hook(map->mlx_win, 2, 1L << 0, ft_keypress, map);
+	mlx_hook(map->mlx_win, 3, 1L << 1, ft_keyrestore, map);
 	if (keycode == A_KEY)
 		printf("%s\n", "down");
 	else if (keycode == W_KEY)
@@ -99,7 +101,9 @@ int				ft_key_hook(int keycode, t_map *map)
 int				ft_exit_game(t_map *map)
 {
 	mlx_destroy_window(map->mlx_ptr, map->mlx_win);
+	map->mlx_ptr = NULL;
 	free(map->mlx_ptr);
+	free(map->mlx_img);
 	exit(0);
 	return (0);
 }
